5b87925526384a7a23b10db9ec0b5077d448cad1
Mariam Sara <mariamsara@totthobox.com>
2026-02-06 11:44:40 +0600

    added nixpacks config for php 8.3

--- storage/framework/views/44f23fff64469c27bb6574ce16c589c9.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $classes = Flux::classes()->add('p-3 rounded-xl')->add('bg-zinc-400/10 dark:bgzinc-400/5');
-?>
-
-<div <?php echo e($attributes->class($classes)); ?> data-flux-card>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /var/www/html/totthobox/resources/views/flux/card/index.blade.php ENDPATH**/ ?>
--- storage/framework/views/a17ecf5628b389a5da654497bed2ec9b.php
+++
@@ -1,5 +0,0 @@
-<tbody <?php echo e($attributes->class(['divide-y divide-zinc-400/25'])); ?>>
-    <?php echo e($slot); ?>
-
-</tbody>
-<?php /**PATH /var/www/html/totthobox/resources/views/flux/table/rows.blade.php ENDPATH**/ ?>
