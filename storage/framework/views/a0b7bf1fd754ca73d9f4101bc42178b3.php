3b0c1eb73fce0abc6652f8e70196f4e70d9a0060
Mariam Sara <mariamsara@totthobox.com>
2026-03-11 12:54:51 +0600

    Remove cache files from git

--- storage/framework/views/a0b7bf1fd754ca73d9f4101bc42178b3.php
+++
@@ -1,3 +0,0 @@
-
-
-<div <?php echo e($attributes->class('flex-1 pointer-events-none')); ?> data-flux-sidebar-spacer></div><?php /**PATH /var/www/html/totthobox/resources/views/flux/sidebar/spacer.blade.php ENDPATH**/ ?>
